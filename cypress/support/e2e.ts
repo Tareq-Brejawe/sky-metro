5b30476fc932fd7b96051eb44352372a533c2e1b
Tareq Brejawe <tareqbrejawe@gmail.com>
2026-09-15 13:32:46 +0300

    Delete cypress directory

--- cypress/support/e2e.ts
+++
@@ -1,9 +0,0 @@
-import './commands'
-import 'cypress-axe'
-describe('Accessibility Test', () => {
-  it('should find no accessibility violations on the home page', () => {
-    cy.visit('/')
-    cy.injectAxe()
-    cy.checkA11y()
-  })
-})
